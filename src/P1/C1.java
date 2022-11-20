8c8eaf8e822bfea197dce8c818fef42af12407bf
sedaakca <sakca35@gmail.com>
2022-11-20 20:14:51 +0300

    deneme

---
+++ src/P1/C1.java
@@ -0,0 +1,10 @@
+package P1;
+
+public class C1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+
+
+}
---
+++ src/P1/c2.java
@@ -0,0 +1,8 @@
+package P1;
+
+public class c2 {
+
+    public static void main(String[] args) {
+        System.out.println("deneme");
+    }
+}
